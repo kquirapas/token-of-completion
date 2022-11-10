1641c7197a04711f233bfab376427bcf7aadfbc9
Kristian Quirapas <kmquirapas@gmail.com>
2022-11-10 17:49:54 +0800

    add: wallet button

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,8 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
   const cardRef = useRef<HTMLImageElement>(null);
   const { contract } = useContract(
-    "0x782Bf3a7B689C615A631EA8F4DE1D348DeBc5483"
+    "0x1f356a9bC08dB319C28863Bc3976bCb14F4f90F4",
+    "signature-drop"
   );
   const { data: metadata } = useContractMetadata(contract);
   const address = useAddress();
@@ -41,32 +42,40 @@ export default function Home() {
     };
 
     setLoading(true);
-    await contract?.erc721.mintTo(address, m);
+    try {
+      // await contract?.createBatch([m, m, m, m, m, m, m, m, m, m]);
+      const tx = await contract?.claimTo(address, 1);
+      console.log(`tx: ${tx}`);
+    } catch (err) {
+      console.log(err);
+    }
     setLoading(false);
   };
 
   return (
-    <main className="min-w-screen flex min-h-screen items-center justify-center bg-hero bg-cover p-8">
-      <div className="flex w-full max-w-[600px] flex-col items-center text-center text-white">
-        <img
-          className="mb-16 w-full drop-shadow-2xl"
-          ref={cardRef}
-          src="/course_completer.png"
-          alt="Course Completer Token"
-        />
-        <h1 className="text-3xl font-bold">Become a Web3 Champion</h1>
-        <p className="mb-4">Get your token of completion below</p>
-        <span className="mb-2">
-          <ConnectWallet />
-        </span>
-        {address && (
-          <button
-            className="w-full max-w-[200px] rounded-xl border-2 border-transparent bg-black-pearl py-4 px-8 font-bold hover:border-white hover:bg-purple-heart"
-            onClick={mint}
-          >
-            {loading ? "Loading..." : "Mint"}
-          </button>
-        )}
+    <main className="min-w-screen bg-hero bg-cover">
+      <div className="flex h-full min-h-screen w-full flex-col items-center justify-center bg-dim p-8">
+        <div className="flex w-full max-w-[600px] flex-col items-center text-center text-white">
+          <img
+            className="mb-16 w-full drop-shadow-2xl"
+            ref={cardRef}
+            src="/course_completer.png"
+            alt="Course Completer Token"
+          />
+          <h1 className="text-3xl font-bold">Become a Web3 Champion</h1>
+          <p className="mb-4">Get your token of completion below</p>
+          <span className="mb-2">
+            <ConnectWallet />
+          </span>
+          {address && (
+            <button
+              className="w-full max-w-[200px] rounded-xl border-2 border-transparent bg-black-pearl py-4 px-8 font-bold hover:border-white hover:bg-purple-heart"
+              onClick={mint}
+            >
+              {loading ? "Loading..." : "Mint"}
+            </button>
+          )}
+        </div>
       </div>
     </main>
   );
